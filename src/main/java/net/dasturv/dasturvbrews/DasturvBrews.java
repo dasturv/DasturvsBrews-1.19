76c977e22fe5bd740cc6d77d1bc9eae771a469d2
dasturv <laeclekander@gmail.com>
2022-07-30 08:03:06 -0500

    Initial commit

---
+++ src/main/java/net/dasturv/dasturvbrews/DasturvBrews.java
@@ -0,0 +1,16 @@
+package net.dasturv.dasturvbrews;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DasturvBrews implements ModInitializer {
+
+	public static final String MOD_ID = "dasturvbrews";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/dasturv/dasturvbrews/DasturvBrewsClient.java
@@ -0,0 +1,10 @@
+package net.dasturv.dasturvbrews;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DasturvBrewsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
